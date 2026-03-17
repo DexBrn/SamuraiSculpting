a9e1d8fd75d302de335a9d63f7357a7302df6fb9
DexBrn <getdex555@gmail.com>
2026-03-17 12:44:50 +0000

    New Voxel Geometry starting to work, can cut balls away from it

--- Assets/Scripts/VoxelMarble.cs
+++ Assets/Scripts/VoxelMarble.cs
@@ -4,28 +4,33 @@ using System.Collections;
 using System.Collections.Generic;
 using EzySlice;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using static UnityEditor.PlayerSettings;
+using Unity.VisualScripting;
+using System.Net.Http.Headers;
 
 
 public class VoxelMarble : MonoBehaviour
 {
 
-    public Vector3 MarbleDimensions = new Vector3(32, 48, 32);
+    public Vector3 MDims = new Vector3(32, 48, 32); //Marble Dimensions
 
     bool[,,] Voxels;
 
     MeshFilter MeshFilter;
+
+    public GameObject TempBall;
 
     void Start()
     {
 
         //Create Solid Block of Marble with X Y Z dimensions determined above
 
-        Voxels = new bool[Mathf.RoundToInt(MarbleDimensions.x), Mathf.RoundToInt(MarbleDimensions.y), Mathf.RoundToInt(MarbleDimensions.z)];
-        MeshFilter = new MeshFilter();
+        Voxels = new bool[Mathf.RoundToInt(MDims.x), Mathf.RoundToInt(MDims.y), Mathf.RoundToInt(MDims.z)];
+        MeshFilter = GetComponent<MeshFilter>();
 
-        for(int x=0;  x<MarbleDimensions.x; x++)
-            for (int y = 0; y < MarbleDimensions.y; y++)
-                for (int z = 0; z < MarbleDimensions.z; z++)
+        for(int x=0;  x<MDims.x; x++)
+            for (int y = 0; y < MDims.y; y++)
+                for (int z = 0; z < MDims.z; z++)
                     Voxels[x, y, z] = true;
         
         GenerateMesh();
@@ -33,7 +38,10 @@ public class VoxelMarble : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CarveSphere(TempBall.transform.position, 5);
+        }
     }
 
     void GenerateMesh()
@@ -42,9 +50,9 @@ public class VoxelMarble : MonoBehaviour
         List<Vector3> Vertices = new List<Vector3>();
         List<int> Triangles = new List<int>();
 
-        for (int x = 0; x < MarbleDimensions.x; x++)
-            for (int y = 0; y < MarbleDimensions.y; y++)
-                for (int z = 0; z < MarbleDimensions.z; z++)
+        for (int x = 0; x < MDims.x; x++)
+            for (int y = 0; y < MDims.y; y++)
+                for (int z = 0; z < MDims.z; z++)
                 {
                     if (!Voxels[x, y, z]) continue;
 
@@ -65,24 +73,134 @@ public class VoxelMarble : MonoBehaviour
     void AddVoxelFaces(int x, int y, int z, List<Vector3> Vertices, List<int> Triangles)
     {
 
+        Vector3 Pos = new Vector3(x, y, z);
+
+        //Add faces on outside of model
+        if (IsEmpty(x+1, y, z))
+            AddFace(Pos, Vector3.right, Vertices, Triangles);
+
+        if (IsEmpty(x - 1, y, z))
+            AddFace(Pos, Vector3.left, Vertices, Triangles);
+
+        if (IsEmpty(x, y + 1, z))
+            AddFace(Pos, Vector3.up, Vertices, Triangles);
+
+        if (IsEmpty(x, y - 1, z))
+            AddFace(Pos, Vector3.down, Vertices, Triangles);
+
+        if (IsEmpty(x, y, z + 1))
+            AddFace(Pos, Vector3.forward, Vertices, Triangles);
+
+        if (IsEmpty(x, y, z - 1))
+            AddFace(Pos, Vector3.back, Vertices, Triangles);
+
+
     }
 
     void AddFace(Vector3 Pos, Vector3 Dir, List<Vector3> Vertices, List<int> Triangles)
     {
 
         int Index = Vertices.Count;
+        Vector3 v0, v1, v2, v3;
 
-        Vertices.Add(Pos);
-        Vertices.Add(Pos + Vector3.up);
-        Vertices.Add(Pos + Vector3.right);
-        Vertices.Add(Pos + Vector3.up + Vector3.right);
+        //Find which direction we need to make a triangle
+        if (true) 
+        {
+            if (Dir == Vector3.up)
+            {
+                v0 = Pos + new Vector3(0, 0, 0);
+                v1 = Pos + new Vector3(0, 0, 1);
+                v2 = Pos + new Vector3(1, 0, 0);
+                v3 = Pos + new Vector3(1, 0, 1);
+            }
+            else if (Dir == Vector3.down)
+            {
+                v0 = Pos + new Vector3(0, 1, 0);
+                v1 = Pos + new Vector3(1, 1, 0);
+                v2 = Pos + new Vector3(0, 1, 1);
+                v3 = Pos + new Vector3(1, 1, 1);
+            }
+            else if (Dir == Vector3.forward)
+            {
+                v0 = Pos + new Vector3(0, 0, 1);
+                v1 = Pos + new Vector3(1, 0, 1);
+                v2 = Pos + new Vector3(0, 1, 1);
+                v3 = Pos + new Vector3(1, 1, 1);
+            }
+            else if (Dir == Vector3.back)
+            {
+                v0 = Pos + new Vector3(0, 0, 0);
+                v1 = Pos + new Vector3(0, 1, 0);
+                v2 = Pos + new Vector3(1, 0, 0);
+                v3 = Pos + new Vector3(1, 1, 0);
+            }
+            else if (Dir == Vector3.right)
+            {
+                v0 = Pos + new Vector3(1, 0, 0);
+                v1 = Pos + new Vector3(1, 1, 0);
+                v2 = Pos + new Vector3(1, 0, 1);
+                v3 = Pos + new Vector3(1, 1, 1);
+            }
+            else // left
+            {
+                v0 = Pos + new Vector3(0, 0, 0);
+                v1 = Pos + new Vector3(0, 0, 1);
+                v2 = Pos + new Vector3(0, 1, 0);
+                v3 = Pos + new Vector3(0, 1, 1);
+            }
+        }
+        
+        //Make Vertices at all of the points determined above
+        Vertices.Add(v0);
+        Vertices.Add(v1);
+        Vertices.Add(v2);
+        Vertices.Add(v3);
+
+        //Make Triangles at the positions
+        Triangles.Add(Index);
+        Triangles.Add(Index + 1);
+        Triangles.Add(Index + 2); 
+
+        Triangles.Add(Index + 2);
+        Triangles.Add(Index + 1);
+        Triangles.Add(Index + 3);
+
 
 
 
     }
     
+    bool IsEmpty(int x, int y, int z)
+    {
+        if (x<0 || y<0 || z<0 || x>=MDims.x || y >= MDims.y || z >= MDims.z) //If coord is outside of bounds of size then we know it IS empty
+            return true;
+
+        return !Voxels[x, y, z]; //Otherwise it is fine
+    }
 
     
+
+    void CarveSphere(Vector3 Centre, float Radius)
+    {
+        Centre = transform.InverseTransformPoint(Centre);
+        print($"CENTRE POS :: {Centre}");
+
+        for (int x = 0; x < MDims.x; x++)
+            for (int y = 0; y < MDims.y; y++)
+                for (int z = 0; z < MDims.z; z++)
+                {
+                    Vector3 Pos = new Vector3(x, y, x); // - new Vector3(MDims.x, MDims.y, MDims.z) / 2;
+                    //print($" {x} {y} {z} POS ::: {Pos}  " );
+                    //print(Vector3.Distance(Pos, Centre));
+
+                    if (Vector3.Distance(Pos, Centre) < Radius)
+                    { Voxels[x, y, z] = false; }
+
+                }
+        GenerateMesh();
+
+    }
+
 
 
 
